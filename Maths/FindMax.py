81775670e262ef8cbd648878ba17f79a5bda2963
vkrm0000 <48318785+vkrm0000@users.noreply.github.com>
2019-03-18 02:39:57 +0900

    Update FindMax.py

--- Maths/FindMax.py
+++ Maths/FindMax.py
@@ -1,14 +1,11 @@
 # NguyenU
 
-def find_max(nums):
+def find_max(nums): # returns the highest integer form given list of integers
     max = nums[0]
     for x in nums:
       if x > max:
         max = x
-    print(max)
+    
+    return max
 
-def main():
-  find_max([2, 4, 9, 7, 19, 94, 5])
 
-if __name__ == '__main__':
-  main()
